c033b412953b9027d62ec26444ad9946d3fa7dba
devlaam <ruud@betaresearch.nl>
2024-02-13 16:29:23 +0100

    Refactored report on Console

--- src/main/scala/s2a/tyrosine/main.scala
+++ src/main/scala/s2a/tyrosine/main.scala
@@ -1,24 +1,28 @@
-package s2a.serine
+package s2a.tyrosine
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExportTopLevel
-import org.scalajs.dom.{document, window, HTMLDivElement, Node}
-
+import org.scalajs.dom.{console, document, window, HTMLDivElement}
 
 object Main :
 
-  private val tyrosineVersion = document.querySelector("tyrosineVersion").asInstanceOf[HTMLDivElement]
+  private def afterLastSemicolon(s: String): Option[String] =
+    val i = s.lastIndexOf(":")
+    if i<0 then None else Some(s.substring(i+1))
+
+  private def reportVersionOnConsole(): Unit =
+    import BuildInfo.*
+    val tyrosineVersion = document.querySelector("#version").asInstanceOf[HTMLDivElement]
+    val changed         = if changes == "0" then "(clean)" else s"(with $changes changes)"
+    val scalaJSversion  = libraryDependencies.find(_.contains("scalajs-library")).flatMap(afterLastSemicolon).getOrElse("unknown")
+    console.info(s"Tyrosine Demo, starting from ${window.location.href}")
+    console.info(s"Application info: version $version, build $buildInfoBuildNumber, commit $commit $changed")
+    console.info(s"Building info: sbt version $sbtVersion, scala version $scalaVersion, scalaJS version $scalaJSversion")
+    console.info(s"Compiler options: ${scalacOptions.mkString(",")}")
+    tyrosineVersion.textContent = s"Tyrosine Demo, version $version, build $buildInfoBuildNumber."
+
 
   @JSExportTopLevel("main")
   def main(args: Array[String]): Unit =
-    import BuildInfo.*
-    def changed = if changes == "0" then "(clean)" else s"(with $changes changes)"
-    val scalaJSversion = libraryDependencies.find(_.contains("scalajs-library")).map(_.lastStr(":")).getOrElse("unknown")
-    val otherLibraries = libraryDependencies.drop(3).map(x => s"${x.midStr(":")} version ${x.lastStr(":")}").mkString(", ")
-    logger.info(s"Tyrosine Demo, starting from ${window.location.href}")
-    logger.info(s"Application info: version $version, build $buildInfoBuildNumber, commit $commit $changed")
-    logger.info(s"Building info: sbt version: $sbtVersion, scala version $scalaVersion, scalaJS version $scalaJSversion")
-    logger.info(s"Libraries: $otherLibraries")
-    logger.info(s"Compiler options: ${scalacOptions.mkString(",")}")
-    tyrosineVersion.textContent = s"Tyrosine Demo, version $version, build $buildInfoBuildNumber."
+    reportVersionOnConsole()
 
